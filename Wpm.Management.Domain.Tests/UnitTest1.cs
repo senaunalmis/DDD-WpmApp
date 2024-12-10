18bb7eae5bf5175799f80b79ed60be8ef399b554
Sena Ünalmış <unalmis.sena@student.atilim.edu.tr>
2024-12-10 21:46:26 +0300

    Entities, Encapsulation, Unit tests

--- Wpm.Management.Domain.Tests/UnitTest1.cs
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -1,10 +1,43 @@
-﻿namespace Wpm.Management.Domain.Tests;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Wpm.Management.Domain.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Pet_should_be_equal()
     {
+        var id = Guid.NewGuid();
+        var pet1 = new Pet(id, "Gofret",8, "yelow", new Weight(20.5m),SexOfPet.Female);
+        var pet2 = new Pet(id, "Bal", 13, "brown", new Weight(30.5m), SexOfPet.Male);
 
+        Assert.True(pet1.Equals(pet2));
+    }
+    [Fact]
+    public void Pet_should_be_equal_using_operators()
+    {
+        var id = Guid.NewGuid();
+        var pet1 = new Pet(id, "Gofret", 8, "yelow", new Weight(20.5m), SexOfPet.Female);
+        var pet2 = new Pet(id, "Bal", 13, "brown", new Weight(30.5m), SexOfPet.Male);
+
+        Assert.True(pet1==pet2);
+    }
+    [Fact]
+    public void Pet_should_not_be_equal_using_operators()
+    {
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+
+        var pet1 = new Pet(id1, "Gofret", 8, "yelow", new Weight(20.5m), SexOfPet.Female);
+        var pet2 = new Pet(id2, "Bal", 13, "brown", new Weight(30.5m), SexOfPet.Male);
+        Assert.True(pet1 != pet2);
+    }
+    [Fact]
+    public void Weight_should_be_equal()
+    {
+        var w1 = new Weight(20.5m);
+        var w2 = new Weight(20.5m);
+
+        Assert.True(w1 == w2);
     }
 }
---
+++ Wpm.Management.Domain/Weight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpm.Management.Domain
+{
+    public record class Weight
+    {
+        // TO DO: Public record kullaımını araştır
+        public decimal Value { get; init; }
+        public Weight(decimal value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Weight value is not valid.");
+            }
+            Value = value;
+
+        }
+
+    }
+}
